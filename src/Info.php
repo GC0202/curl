86259ae69dc6246221498102a12caaa7e5b1d93d
Chaim <gc@dtapp.net>
2020-03-31 17:04:26 +0800

    - 增加返回json

--- src/Info.php
+++ src/Info.php
@@ -21,4 +21,32 @@ class Info extends BasicCurl
         $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
         return $http_type . $_SERVER['HTTP_HOST'] . "/";
     }
+
+    /**
+     * 返回成功
+     * @param array $data
+     * @param string $msg
+     * @param int $code
+     */
+    public function retJsonSuccess(array $data = [], string $msg = 'success', int $code = 0)
+    {
+        date_default_timezone_set('Asia/Shanghai');
+        header('Content-Type:application/json; charset=utf-8');
+        echo json_encode(['code' => $code, 'msg' => $msg, 'time' => time(), 'data' => $data]);
+        exit;
+    }
+
+    /**
+     * 返回失败
+     * @param string $msg
+     * @param int $code
+     * @param array $data
+     */
+    public function retJsonError(string $msg = 'error', int $code = 1, array $data = [])
+    {
+        date_default_timezone_set('Asia/Shanghai');
+        header('Content-Type:application/json; charset=utf-8');
+        echo json_encode(['code' => $code, 'msg' => $msg, 'time' => time(), 'data' => $data]);
+        exit;
+    }
 }
